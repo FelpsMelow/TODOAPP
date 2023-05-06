d8f0e637ccecf6767d766b0510543b3790b6d6a1
Felipe Melo <felipemelo.unidade@gmail.com>
2023-05-06 11:39:53 -0300

    first commit

---
+++ docs/script_login_module.js
@@ -0,0 +1,65 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+import { getAuth, signOut, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js"
+
+import { getFirestore, collection, addDoc, getDoc, doc, onSnapshot} from "https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js"
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyALQDf28nocTbr9e-QrzCX58xj64OdTIis",
+authDomain: "todolist-5e335.firebaseapp.com",
+projectId: "todolist-5e335",
+storageBucket: "todolist-5e335.appspot.com",
+messagingSenderId: "189298719165",
+appId: "1:189298719165:web:a60a2a6fb6d1176276bad4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);  // ------------------------------
+
+const btnLogin = document.querySelector(".btn-login")
+
+
+//Logando
+btnLogin.addEventListener("click", async () => {
+
+    let email = document.querySelector(".email").value
+    let password = document.querySelector(".password").value
+
+    const auth = getAuth();
+
+    await signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in usuario logado
+        let user_log = userCredential.user;
+        console.log(user_log)
+
+        window.location.href = "app.html"
+
+        //document.querySelector(".container-login").style.display =  "block"
+        //document.querySelector(".login").style.display =  "none"
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        //console.log(errorCode)
+
+        //TO DO (Inter travamento nos spans)
+
+        if (errorCode == "auth/invalid-email") {
+            //console.log("Preencha todos os campos")
+            document.querySelector(".warning").style.display =  "none"
+            document.querySelector(".erro").style.display =  "flex"
+
+        }
+        if (errorCode == "auth/wrong-password") {
+            //console.log("Email ou senha incorreto!")
+            document.querySelector(".erro").style.display =  "none"
+            document.querySelector(".warning").style.display =  "flex"
+        }
+
+    });
+
+})
